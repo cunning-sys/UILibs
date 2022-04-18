5a38945ac6e5b39bca4265015344dd563d16b7fc
cunning <76720185+owlsk@users.noreply.github.com>
2022-04-18 15:25:01 -0600

    Update Example.lua

--- LinoriaLib/Example.lua
+++ LinoriaLib/Example.lua
@@ -1,7 +1,4 @@
--- New example script written by wally
--- You can suggest changes with a pull request or something
-
-local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
+local repo = 'https://raw.githubusercontent.com/owlsk/UILibs/main/LinoriaLib/'
 
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
